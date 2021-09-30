4f8c93da97ee314fe8228a1ac47f5a5669b93e81
Jesus Hincapié <jahlon@gmail.com>
2021-09-30 15:24:20 -0500

    Se adiciono la installación de pyside y la copia del archivo pyside2-uic en la carpeta bin de la librería

--- create_venv.bat
+++ create_venv.bat
@@ -1,2 +1,5 @@
 python -m venv ./venv
 .\venv\Scripts\activate
+pip install pyside2
+mkdir .\venv\Lib\site-packages\PySide2\bin
+xcopy .\venv\Scripts\pyside2-uic.exe .\venv\Lib\site-packages\PySide2\bin\uic.exe
