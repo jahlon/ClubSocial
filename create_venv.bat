57b3fa311e2d1315f2b57230f924a705a4010bac
Jesus Hincapié <jahlon@gmail.com>
2021-09-30 22:11:57 -0500

    Se agregar TIMEOUT de 3 segundos

--- create_venv.bat
+++ create_venv.bat
@@ -1,5 +1,6 @@
 python -m venv ./venv
-START /B /wait .\venv\Scripts\activate
+START /B .\venv\Scripts\activate
+TIMEOUT /T 3
 START /wait pip install pyside2
 mkdir .\venv\Lib\site-packages\PySide2\bin
 xcopy .\venv\Scripts\pyside2-uic.exe .\venv\Lib\site-packages\PySide2\bin\uic.exe
