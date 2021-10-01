1749c67cf4e56c865064c3c12827752dffd9bbe3
Jesus Hincapié <jahlon@gmail.com>
2021-09-30 22:57:36 -0500

    Se organiza el comando usando CMD

--- create_venv.bat
+++ create_venv.bat
@@ -1,6 +1,2 @@
 python -m venv ./venv
-START /B .\venv\Scripts\activate
-TIMEOUT /T 3
-START /wait pip install pyside2
-mkdir .\venv\Lib\site-packages\PySide2\bin
-xcopy .\venv\Scripts\pyside2-uic.exe .\venv\Lib\site-packages\PySide2\bin\uic.exe
+CMD /K ".\venv\Scripts\activate & pip install pyside2 & mkdir .\venv\Lib\site-packages\PySide2\bin & echo F|xcopy /S /Q /Y /F .\venv\Scripts\pyside2-uic.exe .\venv\Lib\site-packages\PySide2\bin\uic.exe"
