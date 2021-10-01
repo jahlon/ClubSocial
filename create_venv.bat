46399d6bcac5d3dfdf4258b5f352fc899c02ce55
Jesus Hincapié <jahlon@gmail.com>
2021-09-30 22:02:55 -0500

    Cambiar /wait por /B

--- create_venv.bat
+++ create_venv.bat
@@ -1,5 +1,5 @@
 python -m venv ./venv
-START /wait .\venv\Scripts\activate
+START /B .\venv\Scripts\activate
 START /wait pip install pyside2
 mkdir .\venv\Lib\site-packages\PySide2\bin
 xcopy .\venv\Scripts\pyside2-uic.exe .\venv\Lib\site-packages\PySide2\bin\uic.exe
