0d95a462ae35f4a88079b8077f266f4cc0cf1527
Jesus Hincapié <jahlon@gmail.com>
2021-09-30 21:55:17 -0500

    Se juntan los comandos y se agregan instrucciones para esperar a que termine la activación del venv

--- create_venv.bat
+++ create_venv.bat
@@ -1,5 +1,5 @@
 python -m venv ./venv
-.\venv\Scripts\activate
-pip install pyside2
+START /wait .\venv\Scripts\activate
+START /wait pip install pyside2
 mkdir .\venv\Lib\site-packages\PySide2\bin
 xcopy .\venv\Scripts\pyside2-uic.exe .\venv\Lib\site-packages\PySide2\bin\uic.exe
