14fa4f5e4c0e75278add5d34bc9ad748f16e1d34
s216e15 <jahlon@gmail.com>
2021-10-12 11:30:03 -0500

    Implementamos el requisito para registrar un consumo a un autorizado

--- club_social/vista/gui/gui.py
+++ club_social/vista/gui/gui.py
@@ -4,7 +4,7 @@ from PySide2.QtCore import QRegExp
 from PySide2.QtGui import QRegExpValidator, QIntValidator, QStandardItemModel, QStandardItem
 from PySide2.QtWidgets import QMainWindow, QApplication, QDialog, QMessageBox
 
-from club_social.mundo.errores import SocioExistenteError
+from club_social.mundo.errores import SocioExistenteError, SocioNoExistenteError
 from club_social.mundo.mundo import Club
 from club_social.vista.gui.ui_dialogo_afiliar_socio import Ui_DialogAfiliarSocio
 from club_social.vista.gui.ui_dialogo_registrar_consumo import Ui_DialogRegistrarConsumo
@@ -25,7 +25,16 @@ class VentanaClubSocial(QMainWindow):
         self.ui.pbutton_afiliar_socio.clicked.connect(self.abrir_dialogo_afiliar_socio)
         self.ui.pbutton_registrar_consumo.clicked.connect(self.abrir_dialogo_registrar_consumo)
 
+        self.ui.listview_socios.selectionChanged = self.seleccionar_socio
+
         self.ui.listview_socios.setModel(QStandardItemModel())
+        self.ui.listview_facturas.setModel(QStandardItemModel())
+        # sel_model = self.ui.listview_socios.selectionModel()
+        # sel_model.selectionChanged.connect(self.seleccionar_socio)
+
+        # TODO: Borrar este código
+        for socio in self.club.socios.values():
+            self.actualizar_lista_socios(socio)
 
     def abrir_dialogo_afiliar_socio(self):
         dialogo = DialogoAfiliarSocio(self)
@@ -53,8 +62,68 @@ class VentanaClubSocial(QMainWindow):
         self.ui.listview_socios.model().appendRow(item)
 
     def abrir_dialogo_registrar_consumo(self):
-        dialogo = DialogoRegistrarConsumo(self)
-        resp = dialogo.exec_()
+        indexes = self.ui.listview_socios.selectedIndexes()
+        if len(indexes) == 0:
+            msg_box = QMessageBox(self)
+            msg_box.setWindowTitle("Error")
+            msg_box.setIcon(QMessageBox.Critical)
+            msg_box.setText("Debe seleccionar un socio para registrarle el consumo")
+            msg_box.setStandardButtons(QMessageBox.Ok)
+            msg_box.exec_()
+        else:
+            dialogo = DialogoRegistrarConsumo(self)
+            resp = dialogo.exec_()
+            if resp == QDialog.Accepted:
+                item = self.ui.listview_socios.model().itemFromIndex(indexes[0])
+
+                cedula = item.socio.cedula
+                concepto = dialogo.ui.lineedit_detalle.text()
+                valor = float(dialogo.ui.lineedit_valor.text())
+                autorizado = dialogo.ui.lineedit_autorizado.text()
+
+                try:
+                    self.club.registrar_consumo_a_socio(cedula, concepto, valor, autorizado)
+                except SocioNoExistenteError as err:
+                    msg_box = QMessageBox(self)
+                    msg_box.setWindowTitle("Error registrando consumo")
+                    msg_box.setIcon(QMessageBox.Critical)
+                    msg_box.setText(err.msg)
+                    msg_box.setStandardButtons(QMessageBox.Ok)
+                    msg_box.exec_()
+                else:
+                    self.actualizar_lista_de_facturas()
+
+    def seleccionar_socio(self, selected, deselected):
+        indexes = selected.indexes()
+        if len(indexes) > 0:
+            item = self.ui.listview_socios.model().itemFromIndex(indexes[0])
+
+            self.actualizar_datos_socio(item.socio)
+            self.actualizar_lista_de_facturas()
+            self.actualizar_lista_autorizados()
+
+    def actualizar_datos_socio(self, socio):
+        self.ui.lineedit_cedula.setText(socio.cedula)
+        self.ui.lineedit_nombre.setText(socio.nombre)
+
+    def actualizar_lista_de_facturas(self):
+        self.ui.listview_facturas.model().clear()
+        indexes = self.ui.listview_socios.selectedIndexes()
+        item = self.ui.listview_socios.model().itemFromIndex(indexes[0])
+
+        for factura in item.socio.facturas:
+            item = QStandardItem(str(factura))
+            item.setEditable(False)
+            item.factura = factura
+            self.ui.listview_facturas.model().appendRow(item)
+
+    def actualizar_lista_autorizados(self):
+        self.ui.listwidget_autorizados.clear()
+        indexes = self.ui.listview_socios.selectedIndexes()
+        item = self.ui.listview_socios.model().itemFromIndex(indexes[0])
+
+        for autorizado in item.socio.autorizados:
+            self.ui.listwidget_autorizados.addItem(autorizado)
 
 
 class DialogoAfiliarSocio(QDialog):
