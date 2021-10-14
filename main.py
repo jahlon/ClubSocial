2d3b24b7c49cdc5e9a10e73e3466fe4966c43e78
s216e15 <jahlon@gmail.com>
2021-10-14 11:37:03 -0500

    Se habilitó el código de la rutina de arranque

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 
 from PySide2.QtWidgets import QApplication
 
+from club_social.mundo.mundo import Club
 from club_social.vista.gui.gui import VentanaClubSocial
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = VentanaClubSocial()
+    club = Club()
+    window = VentanaClubSocial(club)
     sys.exit(app.exec_())
